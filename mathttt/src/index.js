0693edc5fa1439a26d392aee00704ad5fedce638
William Guo <william.wenzhou.guo@gmail.com>
2017-05-17 14:49:30 -0400

    1) Add css to the app.css 2) Got rid of the filler stuff in App.js 3) Included redux 4) Separated out the board, game, square, and made a gameContainer

--- mathttt/src/index.js
+++ mathttt/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+// import { tttApp } from './reducers'
 import App from './App';
 import './index.css';
 
+let store = createStore();
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+
+    document.getElementById('root')
 );
